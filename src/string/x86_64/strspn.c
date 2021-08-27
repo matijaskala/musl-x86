f3387d5620cd3e8565ce3478880a3680e308e7f1
Matija Skala <mskala@gmx.com>
2021-08-27 02:05:49 +0200

    another fix

--- src/string/x86_64/strspn.c
+++ src/string/x86_64/strspn.c
@@ -211,10 +211,18 @@ static char* strspn2_fallback(const void *haystack, int n1, int n2) {
 	for (;;) {
 		size_t m1 = *(const size_t*)haystack ^ repeated_n1;
 		size_t m2 = *(const size_t*)haystack ^ repeated_n2;
-		if (((((m1-lowbits) & ~m1) | ((m2-lowbits) & ~m2)) & highbits) != highbits) {
-			for (int i = 0; i < sizeof(size_t); i++)
-				if (((unsigned char*)haystack)[i] != n1 && ((unsigned char*)haystack)[i] != n2)
+		if (m1 != 0 && m2 != 0) {
+			int i = 0;
+			do {
+				int z1 = trailing_zeros(m1) / 8;
+				int z2 = trailing_zeros(m2) / 8;
+				int z = z1 > z2 ? z1 : z2;
+				if (z == 0)
 					return (char*)haystack + i;
+				i += z;
+				m1 >>= z * 8;
+				m2 >>= z * 8;
+			} while (m1 != 0 && m2 != 0 && i < sizeof(size_t));
 		}
 		haystack = (const size_t*)haystack + 1;
 	}
@@ -234,10 +242,19 @@ static char* strspn2_sse2(const void *haystack, int n1, int n2) {
 	while ((size_t)haystack % 16) {
 		uint32_t m1 = *(const uint32_t*)haystack ^ repeated_n1;
 		uint32_t m2 = *(const uint32_t*)haystack ^ repeated_n2;
-		if (((((m1-lowbits) & ~m1) | ((m2-lowbits) & ~m2)) & highbits) != highbits)
-			for (int i = 0; i < sizeof(uint32_t); i++)
-				if (((unsigned char*)haystack)[i] != n1 && ((unsigned char*)haystack)[i] != n2)
+		if (m1 != 0 && m2 != 0) {
+			int i = 0;
+			do {
+				int z1 = trailing_zeros(m1) / 8;
+				int z2 = trailing_zeros(m2) / 8;
+				int z = z1 > z2 ? z1 : z2;
+				if (z == 0)
 					return (char*)haystack + i;
+				i += z;
+				m1 >>= z * 8;
+				m2 >>= z * 8;
+			} while (m1 != 0 && m2 != 0 && i < sizeof(uint32_t));
+		}
 		haystack = (uint32_t*)haystack + 1;
 	}
 	__m128i vn1 = _mm_set1_epi8(n1);
@@ -293,11 +310,20 @@ static char* strspn2_avx2(const void *haystack, int n1, int n2) {
 	while ((size_t)haystack % 16) {
 		uint32_t m1 = *(const uint32_t*)haystack ^ repeated_n1;
 		uint32_t m2 = *(const uint32_t*)haystack ^ repeated_n2;
-		if (((((m1-lowbits) & ~m1) | ((m2-lowbits) & ~m2)) & highbits) != highbits)
-			for (int i = 0; i < sizeof(uint32_t); i++)
-				if (((unsigned char*)haystack)[i] != n1 && ((unsigned char*)haystack)[i] != n2)
+		if (m1 != 0 && m2 != 0) {
+			int i = 0;
+			do {
+				int z1 = trailing_zeros(m1) / 8;
+				int z2 = trailing_zeros(m2) / 8;
+				int z = z1 > z2 ? z1 : z2;
+				if (z == 0)
 					return (char*)haystack + i;
-		haystack = (int32_t*)haystack + 1;
+				i += z;
+				m1 >>= z * 8;
+				m2 >>= z * 8;
+			} while (m1 != 0 && m2 != 0 && i < sizeof(uint32_t));
+		}
+		haystack = (uint32_t*)haystack + 1;
 	}
 	__m128i v16n1 = _mm_set1_epi8(n1);
 	__m128i v16n2 = _mm_set1_epi8(n2);
@@ -340,196 +366,24 @@ static char* strspn2_avx2(const void *haystack, int n1, int n2) {
 	}
 }
 
-static char* strspn3_fallback(const void *haystack, int n1, int n2, int n3) {
-	while ((size_t)haystack % sizeof(size_t)) {
-		if (*(unsigned char*)haystack != n1 && *(unsigned char*)haystack != n2 && *(unsigned char*)haystack != n3)
-			return (void*)haystack;
-		haystack = (char*)haystack + 1;
-	}
-	size_t lowbits = ~(size_t)0 / 0xff;
-	size_t highbits = lowbits * 0x80;
-	size_t repeated_n1 = lowbits * n1;
-	size_t repeated_n2 = lowbits * n2;
-	size_t repeated_n3 = lowbits * n3;
-	for (;;) {
-		size_t m1 = *(const size_t*)haystack ^ repeated_n1;
-		size_t m2 = *(const size_t*)haystack ^ repeated_n2;
-		size_t m3 = *(const size_t*)haystack ^ repeated_n3;
-		if (((((m1-lowbits) & ~m1) | ((m2-lowbits) & ~m2) | ((m3-lowbits) & ~m3)) & highbits) != highbits) {
-			while (*(unsigned char*)haystack == n1 || *(unsigned char*)haystack == n2 || *(unsigned char*)haystack == n3)
-				haystack = (char*)haystack + 1;
-			return (void*)haystack;
-		}
-		haystack = (const size_t*)haystack + 1;
-	}
-}
-
-__attribute__((__target__("sse2")))
-static char* strspn3_sse2(const void *haystack, int n1, int n2, int n3) {
-	while ((size_t)haystack % sizeof(size_t)) {
-		if (*(unsigned char*)haystack != n1 && *(unsigned char*)haystack != n2 && *(unsigned char*)haystack != n3)
-			return (void*)haystack;
-		haystack = (char*)haystack + 1;
-	}
-	uint32_t lowbits = ~(uint32_t)0 / 0xff;
-	uint32_t highbits = lowbits * 0x80;
-	uint32_t repeated_n1 = lowbits * n1;
-	uint32_t repeated_n2 = lowbits * n2;
-	uint32_t repeated_n3 = lowbits * n3;
-	while ((size_t)haystack % 16) {
-		uint32_t m1 = *(const uint32_t*)haystack ^ repeated_n1;
-		uint32_t m2 = *(const uint32_t*)haystack ^ repeated_n2;
-		uint32_t m3 = *(const uint32_t*)haystack ^ repeated_n3;
-		if (((((m1-lowbits) & ~m1) | ((m2-lowbits) & ~m2) | ((m3-lowbits) & ~m3)) & highbits) != highbits) {
-			while (*(unsigned char*)haystack == n1 || *(unsigned char*)haystack == n2 || *(unsigned char*)haystack == n3)
-				haystack = (char*)haystack + 1;
-			return (void*)haystack;
-		}
-		haystack = (uint32_t*)haystack + 1;
-	}
-	__m128i vn1 = _mm_set1_epi8(n1);
-	__m128i vn2 = _mm_set1_epi8(n2);
-	__m128i vn3 = _mm_set1_epi8(n3);
-	if ((size_t)haystack % 32) {
-		__m128i x = _mm_load_si128(haystack);
-		__m128i eq1 = _mm_cmpeq_epi8(x, vn1);
-		__m128i eq2 = _mm_cmpeq_epi8(x, vn2);
-		__m128i eq3 = _mm_cmpeq_epi8(x, vn3);
-		if (_mm_movemask_epi8(_mm_or_si128(_mm_or_si128(eq1, eq2), eq3)) != 0xffff) {
-			int mask1 = _mm_movemask_epi8(eq1);
-			int mask2 = _mm_movemask_epi8(eq2);
-			int mask3 = _mm_movemask_epi8(eq3);
-			return (char*)haystack + trailing_zeros(~mask1 & ~mask2 & ~mask3);
-		}
-		haystack = (const __m128i*)haystack + 1;
-	}
-	const __m128i *ptr = haystack;
-	for (;;) {
-		__m128i a = _mm_load_si128(ptr);
-		__m128i b = _mm_load_si128(ptr+1);
-		__m128i eqa1 = _mm_cmpeq_epi8(vn1, a);
-		__m128i eqb1 = _mm_cmpeq_epi8(vn1, b);
-		__m128i eqa2 = _mm_cmpeq_epi8(vn2, a);
-		__m128i eqb2 = _mm_cmpeq_epi8(vn2, b);
-		__m128i eqa3 = _mm_cmpeq_epi8(vn3, a);
-		__m128i eqb3 = _mm_cmpeq_epi8(vn3, b);
-		__m128i or1 = _mm_or_si128(eqa1, eqa2);
-		__m128i or2 = _mm_or_si128(or1, eqa3);
-		__m128i or3 = _mm_or_si128(eqb1, eqb2);
-		__m128i or4 = _mm_or_si128(or3, eqb3);
-		__m128i or5 = _mm_and_si128(or2, or4);
-
-		if (_mm_movemask_epi8(or5) != 0xffff) {
-			int mask1 = _mm_movemask_epi8(eqa1);
-			int mask2 = _mm_movemask_epi8(eqa2);
-			int mask3 = _mm_movemask_epi8(eqa3);
-			if (mask1 != 0xffff || mask2 != 0xffff || mask3 != 0xffff)
-				return (char*)ptr + trailing_zeros(~mask1 & ~mask2 & ~mask3);
-			mask1 = _mm_movemask_epi8(eqb1);
-			mask2 = _mm_movemask_epi8(eqb2);
-			mask3 = _mm_movemask_epi8(eqb3);
-			return (char*)(ptr+1) + trailing_zeros(~mask1 & ~mask2 & ~mask3);
-		}
-		ptr += 2;
-	}
-}
-
-__attribute__((__target__("avx2")))
-static char* strspn3_avx2(const void *haystack, int n1, int n2, int n3) {
-	while ((size_t)haystack % sizeof(size_t)) {
-		if (*(unsigned char*)haystack != n1 && *(unsigned char*)haystack != n2 && *(unsigned char*)haystack != n3)
-			return (void*)haystack;
-		haystack = (char*)haystack + 1;
-	}
-	uint32_t lowbits = ~(uint32_t)0 / 0xff;
-	uint32_t highbits = lowbits * 0x80;
-	uint32_t repeated_n1 = lowbits * n1;
-	uint32_t repeated_n2 = lowbits * n2;
-	uint32_t repeated_n3 = lowbits * n3;
-	while ((size_t)haystack % 16) {
-		uint32_t m1 = *(const uint32_t*)haystack ^ repeated_n1;
-		uint32_t m2 = *(const uint32_t*)haystack ^ repeated_n2;
-		uint32_t m3 = *(const uint32_t*)haystack ^ repeated_n3;
-		if (((((m1-lowbits) & ~m1) | ((m2-lowbits) & ~m2) | ((m3-lowbits) & ~m3)) & highbits) != highbits) {
-			while (*(unsigned char*)haystack == n1 || *(unsigned char*)haystack == n2 || *(unsigned char*)haystack == n3)
-				haystack = (char*)haystack + 1;
-			return (void*)haystack;
-		}
-		haystack = (uint32_t*)haystack + 1;
-	}
-	__m128i v16n1 = _mm_set1_epi8(n1);
-	__m128i v16n2 = _mm_set1_epi8(n2);
-	__m128i v16n3 = _mm_set1_epi8(n3);
-	while ((size_t)haystack % 64) {
-		__m128i x = _mm_load_si128(haystack);
-		__m128i eq1 = _mm_cmpeq_epi8(x, v16n1);
-		__m128i eq2 = _mm_cmpeq_epi8(x, v16n2);
-		__m128i eq3 = _mm_cmpeq_epi8(x, v16n3);
-		if (_mm_movemask_epi8(_mm_or_si128(_mm_or_si128(eq1, eq2), eq3)) != 0xffff) {
-			int mask1 = _mm_movemask_epi8(eq1);
-			int mask2 = _mm_movemask_epi8(eq2);
-			int mask3 = _mm_movemask_epi8(eq3);
-			return (char*)haystack + trailing_zeros(~mask1 & ~mask2 & ~mask3);
-		}
-		haystack = (const __m128i*)haystack + 1;
-	}
-	__m256i vn1 = _mm256_set1_epi8(n1);
-	__m256i vn2 = _mm256_set1_epi8(n2);
-	__m256i vn3 = _mm256_set1_epi8(n2);
-	const __m256i *ptr = haystack;
-	for (;;) {
-		__m256i a = _mm256_load_si256(ptr);
-		__m256i b = _mm256_load_si256(ptr+1);
-		__m256i eqa1 = _mm256_cmpeq_epi8(vn1, a);
-		__m256i eqb1 = _mm256_cmpeq_epi8(vn1, b);
-		__m256i eqa2 = _mm256_cmpeq_epi8(vn2, a);
-		__m256i eqb2 = _mm256_cmpeq_epi8(vn2, b);
-		__m256i eqa3 = _mm256_cmpeq_epi8(vn3, a);
-		__m256i eqb3 = _mm256_cmpeq_epi8(vn3, b);
-		__m256i or1 = _mm256_or_si256(eqa1, eqa2);
-		__m256i or2 = _mm256_or_si256(or1, eqa3);
-		__m256i or3 = _mm256_or_si256(eqb1, eqb2);
-		__m256i or4 = _mm256_or_si256(or3, eqb3);
-		__m256i or5 = _mm256_and_si256(or2, or4);
-
-		if (_mm256_movemask_epi8(or5) != -1) {
-			int mask1 = _mm256_movemask_epi8(eqa1);
-			int mask2 = _mm256_movemask_epi8(eqa2);
-			int mask3 = _mm256_movemask_epi8(eqa3);
-			if (mask1 != -1 || mask2 != -1 || mask3 != -1)
-				return (char*)ptr + trailing_zeros(~mask1 & ~mask2 & ~mask3);
-			mask1 = _mm256_movemask_epi8(eqb1);
-			mask2 = _mm256_movemask_epi8(eqb2);
-			mask3 = _mm256_movemask_epi8(eqb3);
-			return (char*)(ptr+1) + trailing_zeros(~mask1 & ~mask2 & ~mask3);
-		}
-		ptr += 2;
-	}
-}
-
 static char *strspn1_auto(const void *, int);
 static char *strspn2_auto(const void *, int, int);
-static char *strspn3_auto(const void *, int, int, int);
 
 static char *(*strspn1_impl)(const void *, int) = strspn1_auto;
 static char *(*strspn2_impl)(const void *, int, int) = strspn2_auto;
-static char *(*strspn3_impl)(const void *, int, int, int) = strspn3_auto;
 
 static void strspn_init() {
 	if (has_avx2()) {
 		strspn1_impl = strspn1_avx2;
 		strspn2_impl = strspn2_avx2;
-		strspn3_impl = strspn3_avx2;
 	}
 	else if (has_sse2()) {
 		strspn1_impl = strspn1_sse2;
 		strspn2_impl = strspn2_sse2;
-		strspn3_impl = strspn3_sse2;
 	}
 	else {
 		strspn1_impl = strspn1_fallback;
 		strspn2_impl = strspn2_fallback;
-		strspn3_impl = strspn3_fallback;
 	}
 }
 static char *strspn1_auto(const void *haystack, int n1) {
@@ -552,13 +406,11 @@ size_t strspn(const char *s, const char *accept) {
 		return strspn1_impl(s, (unsigned char)accept[0]) - s;
 	else if (!accept[2])
 		return strspn2_impl(s, (unsigned char)accept[0], (unsigned char)accept[1]) - s;
-	else if (!accept[3])
-		return strspn3_impl(s, (unsigned char)accept[0], (unsigned char)accept[1], (unsigned char)accept[2]) - s;
 	uint64_t byteset[4] = {0};
 	while (*accept) {
 		byteset[(unsigned char)*accept/64] |= (uint64_t)1 << (unsigned char)*accept%64;
 		accept++;
-    }
+	}
 	size_t i = 0;
 	while (byteset[(unsigned char)s[i]/64] & (uint64_t)1 << (unsigned char)s[i]%64)
 		i++;
